826579487c6f9ae67fea0310713f0b71fa7f2484
Erika <erika@erikaheidi.com>
2024-12-04 14:53:16 +0100

    another try at lintfixing the file

--- go/go-digester/main.go
+++ go/go-digester/main.go
@@ -1,26 +1,29 @@
 package main
 
 import (
-	"fmt"
+    "fmt"
     "flag"
-	"github.com/google/go-containerregistry/pkg/name"
-	"github.com/google/go-containerregistry/pkg/v1/remote"
+    "github.com/google/go-containerregistry/pkg/name"
+    "github.com/google/go-containerregistry/pkg/v1/remote"
 )
 
 func main() {
     flag.Parse()
-	image := "go"
-	args := flag.Args()
-	if len(args) >= 1 && args[0] != "" {
-    	image = args[0]
+    image := "go"
+    args := flag.Args()
+    if len(args) >= 1 && args[0] != "" {
+        image = args[0]
     }
-	ref, err := name.ParseReference("cgr.dev/chainguard/" + image)
-	if err != nil {
-		panic(err)
-	}
-	desc, err := remote.Get(ref)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("The latest digest of the %s Chainguard Image is %s\n", image, desc.Digest)
+
+    ref, err := name.ParseReference("cgr.dev/chainguard/" + image)
+    if err != nil {
+        panic(err)
+    }
+
+    desc, err := remote.Get(ref)
+    if err != nil {
+        panic(err)
+    }
+
+    fmt.Printf("The latest digest of the %s Chainguard Image is %s\n", image, desc.Digest)
 }
